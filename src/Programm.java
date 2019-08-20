d26c8bd1c32459b3ac525a60851ec000c6072726
marrionmor <masha_914@mail.ru>
2019-08-20 23:14:02 +0300

    создание

--- src/Programm.java
+++ src/Programm.java
@@ -2,5 +2,6 @@ public class Programm {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("What's happened?");
+        System.out.println("fjfjfj");
     }
 }
