993b6e2195f0a06156816eff21b5d7cccb0330e5
marrionmor <masha_914@mail.ru>
2019-08-20 23:04:43 +0300

    создание

--- src/Programm.java
+++ src/Programm.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Programm {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("What's happened?");
+    }
 }
